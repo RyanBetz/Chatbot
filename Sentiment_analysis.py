4a2870a176d6c8dbafb7a8c1a5c7bcb023d0d156
RyanBetz <ryanbetz8484@gmail.com>
2022-12-10 17:59:47 -0500

    practice

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -2,40 +2,54 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 import spacy as sp
 from nltk import word_tokenize
-import nltk
-
+from sklearn.model_selection import train_test_split
+from wordcloud import STOPWORDS
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+#include other stopwords and numbers to remove
+stop_words = set(STOPWORDS)
+stop_words.add("https")
+stop_words.add("co")
+stop_words.add("rt")
+stop_words.add("RT")
+stop_words.add("14")
 #create csv with onl the column 3 in twitter_info.csv
 df = pd.read_csv('Twitter_info.csv', usecols=[2])
 #iterate through each row in the csv
-for index, row in df.iterrows():
-    #identify if each row contains a positive or negative word and add a column to the csv
+def classify_sentiment(row):
     if 'good' in row['Tweet']:
-        df.loc[index, 'Sentiment'] = 'Positive'
+        return 'Positive'
     elif 'bad' in row['Tweet']:
-        df.loc[index, 'Sentiment'] = 'Negative'
+        return 'Negative'
     else:
-        df.loc[index, 'Sentiment'] = 'Neutral'
-#save the csv
-df.to_csv('Twitter_info_3.csv')
-#count the number of positive, negative, and neutral tweets
-print(df['Sentiment'].value_counts())
+        return 'Neutral'
 
-#use n-grams to find the most common words
-vect = CountVectorizer(ngram_range=(1,2), max_features=10)
+df['Sentiment'] = df.apply(classify_sentiment, axis=1)
+#use n-grams to find the most common words and remove stop words
+vect = CountVectorizer(ngram_range=(1,2), max_features=10, stop_words=stop_words)
 vect.fit(df['Tweet'])
 vect_transform = vect.transform(df['Tweet'])
 vect_df = pd.DataFrame(vect_transform.toarray(), columns=vect.get_feature_names_out())
 print(vect_df)
+#count the number o f positive, negative, and neutral tweets
+print(df['Sentiment'].value_counts())
 #tokenize the tweets using word tokenize
 tokens_tweets = [word_tokenize(tweet) for tweet in df['Tweet']]
 #iterate through each tokenized tweet and create new feature for the length of each tweets
 for index, tweet in enumerate(tokens_tweets):
     df.loc[index, 'Length'] = len(tweet)
+#lemmatize the tweets
+nlp = sp.load('en_core_web_sm')
+lemmatized_tweets = []
+for tweet in df['Tweet']:
+    lemmatized_tweets.append([token.lemma_ for token in nlp(tweet)])
+df['Predicted'] = y_pred
 #save the csv
 df.to_csv('Twitter_info_3.csv')
-#find the average length of the tweets
-print(df['Length'].mean())
-
 
 
 
