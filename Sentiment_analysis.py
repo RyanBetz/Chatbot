8e4924279d00de6de293849f5e565e7e12d7b53f
RyanBetz <ryanbetz8484@gmail.com>
2022-12-11 17:42:16 -0500

    practice

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -14,16 +14,17 @@ stop_words.add("RT")
 stop_words.add("14")
 #create csv with onl the column 3 in twitter_info.csv
 df = pd.read_csv('Twitter_info.csv', usecols=[2])
+#read twitter_info_4.csv
+df_2 = pd.read_csv('Twitter_info_4.csv')
 #iterate through each row in the csv
-def classify_sentiment(row):
-    if 'good' in row['Tweet']:
-        return 'Positive'
-    elif 'bad' in row['Tweet']:
-        return 'Negative'
-    else:
-        return 'Neutral'
-
-df['Sentiment'] = df.apply(classify_sentiment, axis=1)
+#def classify_sentiment(row):
+#    if 'good' in row['Tweet']:
+#        return 'Positive'
+#    elif 'bad' in row['Tweet']:
+#        return 'Negative'
+#    else:
+#        return 'Neutral'
+#df['Sentiment'] = df.apply(classify_sentiment, axis=1)
 #use n-grams to find the most common words and remove stop words
 #vect = CountVectorizer(ngram_range=(1,2), max_features=10, stop_words=stop_words)
 #vect.fit(df['Tweet'])
@@ -31,22 +32,17 @@ df['Sentiment'] = df.apply(classify_sentiment, axis=1)
 #vect_df = pd.DataFrame(vect_transform.toarray(), columns=vect.get_feature_names_out())
 #print(vect_df)
 #count the number o f positive, negative, and neutral tweets
-print(df['Sentiment'].value_counts())
+#print(df['Sentiment'].value_counts())
 #tokenize the tweets using word tokenize
 tokens_tweets = [word_tokenize(tweet) for tweet in df['Tweet']]
-#iterate through each tokenized tweet and create new feature for the length of each tweets
 #lemmatize the tweets
 nlp = sp.load('en_core_web_sm')
-lemmatized_tweets = []
+lemmatized_tweets_1 = []
 for tweet in df['Tweet']:
-    lemmatized_tweets.append([token.lemma_ for token in nlp(tweet)])
+    lemmatized_tweets_1.append([token.lemma_ for token in nlp(tweet)])
 #remove all stop words and numbers from lemmatized tweets
-for index, tweet in enumerate(lemmatized_tweets):
-    lemmatized_tweets[index] = [word for word in tweet if word not in stop_words and not word.isnumeric()]
-#perform sentiment analysis on the lemmatized tweets
-#vectorize the training and testing data
-
-
+for index, tweet in enumerate(lemmatized_tweets_1):
+    lemmatized_tweets_1[index] = [word for word in tweet if word not in stop_words and not word.isnumeric()]
 #save the csv
 df.to_csv('Twitter_info_3.csv')
 
--- Twitter_Training_Data.py
+++ Twitter_Training_Data.py
@@ -1,10 +1,18 @@
-
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk import word_tokenize
 #import twitter training data
 see = pd.read_csv(r"C:\Users\ryanb\OneDrive\tweets_data.csv", encoding = 'latin-1')
 #change when label column is 1 to negative
 see.loc[see['label'] == 1, 'label'] = 'Negative'
 #change when label column is 0 to other
 see.loc[see['label'] == 0, 'label'] = 'Positive/Neutral'
+#vectorize the tweets
+vect = CountVectorizer(ngram_range=(1,2), max_features=10)
+vect.fit(see['tweet'])
+vect_transform = vect.transform(see['tweet'])
+vect_df = pd.DataFrame(vect_transform.toarray(), columns=vect.get_feature_names_out())
+#tokenize the tweets
+tokens_tweets = [word_tokenize(tweet) for tweet in see['tweet']]
 #save the csv
 see.to_csv('Twitter_info_4.csv')
