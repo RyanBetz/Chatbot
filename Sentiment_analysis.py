9ee63a14a493cc7c568fb56cc6f58cbf596d8053
ryanbetz <ryanbetz8484@gmail.com>
2023-01-29 17:17:25 -0500

    practice

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -16,5 +16,10 @@ with open('Twitter_info_2.csv', 'r') as file:
         for row in reader:
             tweet = row['Tweet']
             sentiment = sia.polarity_scores(tweet)
-            row['Sentiment'] = sentiment
+            if sentiment['compound'] >= 0.05:
+                row['Sentiment'] = 'positive'
+            elif sentiment['compound'] <= -0.05:
+                row['Sentiment'] = 'negative'
+            else:
+                row['Sentiment'] = 'neutral'
             writer.writerow(row)
