aa5182cd80db4fe955d89903f825c83ea3c5a0e7
ryanbetz <ryanbetz8484@gmail.com>
2023-01-29 23:14:51 -0500

    practice

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -1,31 +1,31 @@
-import csv
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
 import pandas as pd
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 # Initialize SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
 
-# Open the input file in read mode
-with open('Twitter_info_2.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    fieldnames = reader.fieldnames + ['Sentiment']
-    # Open the output file in write mode
-    with open('Twitter_info_2.csv', 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in reader:
-            tweet = row['Tweet']
-            sentiment = sia.polarity_scores(tweet)
-            if sentiment['compound'] >= 0.05:
-                row['Sentiment'] = 'positive'
-            elif sentiment['compound'] <= -0.05:
-                row['Sentiment'] = 'negative'
-            else:
-                row['Sentiment'] = 'neutral'
-            writer.writerow(row)
-#generate summary statistics of the sentiment
-#delete the last column
+# Read the input file
 df = pd.read_csv('Twitter_info_2.csv')
-df = df.drop(columns=['Sentiment'])
-#save the file
-df.to_csv('Twitter_info_2.csv')
+
+# Check if the "Sentiment" column already exists
+if "Sentiment" in df.columns:
+    # If it does, drop the column
+    df = df.drop(columns=["Sentiment"])
+
+# Add a new column "Sentiment"
+df["Sentiment"] = None
+
+# Analyze the sentiment of each tweet
+for i in range(len(df)):
+    tweet = df.loc[i, "Tweet"]
+    sentiment = sia.polarity_scores(tweet)
+    if sentiment['compound'] >= 0.05:
+        sentiment_score = 'positive'
+    elif sentiment['compound'] <= -0.05:
+        sentiment_score = 'negative'
+    else:
+        sentiment_score = 'neutral'
+    df.loc[i, "Sentiment"] = sentiment_score
+
+# Save the output file
+df.to_csv('Twitter_info_2.csv', index=False)
